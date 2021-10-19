781aa0bfc189e4f9bc61881af79c5a4b7539a7ee
Will Gilbert <gilbert@informagen.com>
2021-10-19 14:29:48 -0400

    Initial Commit

---
+++ Sequence Analysis/Sequence_AnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sequence_AnalysisApp.swift
+//  Sequence Analysis
+//
+//  Created by Will Gilbert on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Sequence_AnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sequence AnalysisUITests/Sequence_AnalysisUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Sequence_AnalysisUITestsLaunchTests.swift
+//  Sequence AnalysisUITests
+//
+//  Created by Will Gilbert on 10/19/21.
+//
+
+import XCTest
+
+class Sequence_AnalysisUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
