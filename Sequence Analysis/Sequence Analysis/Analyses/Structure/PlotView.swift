f544ffbf1a0f10ec671faa7df800d37fe951b3e0
Will Gilbert <gilbert@informagen.com>
2022-01-21 18:02:21 -0500

    Add zoom and pan the Stucture analysis plot

--- Sequence Analysis/Sequence Analysis/Analyses/Structure/PlotView.swift
+++ Sequence Analysis/Sequence Analysis/Analyses/Structure/PlotView.swift
@@ -30,54 +30,92 @@ struct PlotData {
 
 struct PlotView: View {
   
+  @State private var scale: Double = 1.0
+  
   let plotData: PlotData
     
   var body: some View {
-
-    // Convert Doubles to CGFloats for Drawing methods
+    
+    // Convert Doubles to CGFloats for the drawing methods
     let lower = CGFloat(plotData.lower)
     let upper = CGFloat(plotData.upper)
     let cutoff = CGFloat(plotData.cutoff)
-    let length = CGFloat(plotData.length)
+    let extent = CGFloat(plotData.length)
 
-    return VStack {
-      GeometryReader { reader in
+    return GeometryReader { g in
+    
+      // Generate constant for the zooming and scrolling
+      let height = g.size.height
+      let panelWidth = g.size.width
+      var minScale = panelWidth/extent
+      let maxScale = minScale * log2(extent)
+      let scrollViewWidth = extent * scale
+      
+      VStack(alignment: .leading) {
         
-        //  Calculate position of each datum position on the frame width
-        //  Calculate height of graph based on the frame height
-        let positionWidth = reader.size.width /  length
-        let dataRange = reader.size.height / (upper - lower)
-        let cutoffPx = (cutoff - lower) * dataRange
+        HStack (spacing: 15) {
+          Slider(
+            value: $scale,
+            in: minScale...maxScale
+          ).disabled(minScale >= maxScale)
 
-        // Draw each datum measurement
-        ForEach(plotData.data, id: \.self) { datum in
-         
-          // Draw the value as a rectangle
-          Path { path in
-
-            // Calculate the horizontal position for this datum
-            let positionPx = datum.position * positionWidth
-            
-            // Calculate the lower and upper for this datum
-            let valuePx = (datum.value  - lower) * dataRange
-            
-            // Draw this datum as a rectangle from the cutoff to its value
-            let origin = CGPoint(x: positionPx, y: reader.size.height - cutoffPx)
-            let size = CGSize(width: positionWidth, height: cutoffPx - valuePx)
-            path.addRect(CGRect(origin: origin, size: size))
-            
-            // Visualize the drawn rectanges overlayed with a color gradient
-          }.fill(plotData.gradient)
+          Text("Scale: \(F.f(scale, decimal: 2))")
         }
         
-        // Draw the reference cutoff line in gray; Should blend into the data
-        Path { p in
-          p.move(to: .init(x: 0, y: reader.size.height - cutoffPx))
-          p.addLine(to: .init(x: length * positionWidth, y: reader.size.height - cutoffPx))
-        }.stroke(.gray, lineWidth: 1.0)
+        // Calculate each datum position on the panel width
+        // Calculate height of each datum based on the panel height
+        let positionWidth = scale //(panelWidth / extent)
+        let dataRange = height / (upper - lower)
+        let cutoffPx = (cutoff - lower) * dataRange
+          
+        ScrollView( .horizontal, showsIndicators: true) {
+
+            // ZStack is very important here!
+            ZStack(alignment: .topLeading) {
               
+              // Draw each datum measurement and a rectangle
+              ForEach(plotData.data, id: \.self) { datum in
+               
+                // Draw the value as a rectangle
+                Path { path in
+
+                  // Calculate the horizontal position for this datum
+                  let positionPx = datum.position * positionWidth
+                  
+                  // Calculate the lower and upper for this datum
+                  let valuePx = (datum.value  - lower) * dataRange
+                  
+                  // Draw this datum as a rectangle from the cutoff to its value
+                  let origin = CGPoint(x: positionPx, y: height - cutoffPx)
+                  let size = CGSize(width: positionWidth, height: cutoffPx - valuePx)
+                  path.addRect(CGRect(origin: origin, size: size))
+                  
+                  // Visualize the drawn rectanges overlayed with a color gradient
+                }.fill(plotData.gradient)
+              }
+              
+              // Draw the reference cutoff line in gray; Should blend into the data rectangles
+              Path { p in
+                p.move(to: .init(x: 0, y: height - cutoffPx))
+                p.addLine(to: .init(x: extent * positionWidth, y: height - cutoffPx))
+              }.stroke(.gray, lineWidth: 1.0)
+            
+          // Panel size for ScrollView
+          }.frame(width: scrollViewWidth, height: height)
+          
+        }.onAppear {
+            // Set the inital zoom, that is, scale
+            let panelWidth = g.size.width
+            scale = panelWidth/extent
+          }.onChange(of: g.frame(in: .global).width) { value in
+            // Change the zoom if the panel size changes
+            minScale = value/Double(extent)
+            scale = scale > minScale ? scale : minScale
+          }
+        
       }
-    }.padding()
+    }.padding(EdgeInsets(top: 0, leading: 0, bottom: 50, trailing: 0))
   }
+      
 }
 
